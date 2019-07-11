dc848b0655eef6d982e5e529256df1c8a6a3a1ed
Mohsen Abdollahi <mohsen9april@gmail.com>
2019-07-11 17:50:46 +0430

    Create TableView and 2BarButton

--- SlideOutMenu/ViewController.swift
+++ SlideOutMenu/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationItems()
+        tableView.backgroundColor = .red
     }
-
-
+    
+    fileprivate func  setupNavigationItems(){
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: UIBarButtonItem.Style.plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: UIBarButtonItem.Style.plain, target: self, action: #selector(handleHide))
+    }
+    @objc func handleOpen(){
+        print("Opening menu is...")
+    }
+    @objc func handleHide(){
+       print("Hide is clicking....")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cellId")
+        cell.textLabel?.text = "Row : \(indexPath.row)"
+        return cell
+    }
 }
-
